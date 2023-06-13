1ef71678c07a79e63673bf91a08c61a749c97570
TinMon11 <tincho_mondino@hotmail.com>
2023-06-13 20:34:57 +0200

    React Helmet

--- src/pages/AllContents.js
+++ src/pages/AllContents.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Main from "./main/main";
 import Servicios from "./servicios/servicios";
 import Nosotros from "./nosotros/nosotros";
@@ -16,6 +17,22 @@ import Preguntas from "./preguntas/preguntas";
 const AllContents = () => {
   return (
     <>
+      <Helmet>
+        <title>Agencia de Marketing Digital - Alcance Creativo</title>
+        <meta
+          name="description"
+          content="Alcance Creativo ofrece servicios profesionales de marketing digital para empresas y emprendedores. Ofrecemos campañas de publicidad en redes sociales, SEO, diseño web y más."
+        />
+        <meta
+          name="keywords"
+          content="Agencia Marketing digital, TiendaNube, Facebook Ads, Google Ads, redes sociales, tiendas online, Instagram Ads, Publicidad Digital, Redes Sociales, Community Manager"
+        />
+        <meta property="og:title" content="Agencia de Marketing Digital - ALcance Creativo" />
+        <meta
+          property="og:description"
+          content="Aumenta tus Ventas Online. Somos expertos en Campañas de Facebook Ads, Google Ads, gestión de redes sociales y creación de tiendas online."
+        />
+      </Helmet>
       <Main />
       <Servicios />
       <Nosotros />
@@ -25,9 +42,9 @@ const AllContents = () => {
       <GoogleDetails />
       <RedesHome />
       <RedesDetails />
-      <TiendaOnlineHome/>
+      <TiendaOnlineHome />
       <TiendaDetails />
-      <Contactanos/>
+      <Contactanos />
       <Preguntas />
     </>
   );
