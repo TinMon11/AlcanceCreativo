b8cb7888f7d3dbec8ea7ea1b3f3e70f7b79a8175
TinMon11 <tincho_mondino@hotmail.com>
2023-06-20 18:02:09 +0200

    Import Helmet

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '../src/styles/index.css';
 import '../src/styles/fonts.css';
 import App from './App';
+import { Helmet } from 'react-helmet';
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
