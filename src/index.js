3cc18a9fb07ab371b312043ef3d92f3e08bba754
TinMon11 <tincho_mondino@hotmail.com>
2023-06-20 17:55:39 +0200

    Add FBPixel

--- src/index.js
+++ src/index.js
@@ -12,6 +12,25 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <script>{`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '790603602660671');
+        fbq('track', 'PageView');
+      `}</script>
+      <noscript>{`
+        <img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=790603602660671&ev=PageView&noscript=1"
+        />
+      `}</noscript>
+    </Helmet>
     <App />
   </React.StrictMode>
 );
